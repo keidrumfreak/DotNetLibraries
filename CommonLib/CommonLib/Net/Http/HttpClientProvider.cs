824991bba8ef6eebe2f9aa0655f6bdcca9aa112c
keidrumfreak <kei.drumfreak@gmail.com>
2020-11-06 19:05:53 +0900

    Webページ情報取得クラス作成

---
+++ CommonLib/CommonLib/Net/Http/HttpClientProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLib.Net
+{
+    public class HttpClientProvider
+    {
+        private static HttpClient client;
+        public static HttpClient GetClient()
+        {
+            return client ?? (client = new HttpClient());
+        }
+    }
+}
---
+++ CommonLib/CommonLib/Net/Http/WebPage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Sgml;
+
+namespace CommonLib.Net.Http
+{
+    public class WebPage
+    {
+        public XDocument Content { get; private set; }
+
+        public Uri Uri { get; }
+
+        public WebPage(string uri)
+        {
+            Uri = new Uri(uri);
+        }
+
+        public virtual async Task LoadAsync()
+        {
+            var content = await HttpClientProvider.GetClient().GetStringAsync(Uri);
+
+            using var stream = new StringReader(content);
+            using var sgml = new SgmlReader { DocType = "HTML", CaseFolding = CaseFolding.ToLower, InputStream = stream };
+
+            Content = XDocument.Load(sgml);
+        }
+    }
+}
